d3205b58d7ee3186e4d5dac9998a6e58d998e1f6
homow <homowsh897@gmail.com>
2026-01-04 22:09:38 +0330

    feat - add user model

---
+++ src/models/User.model.ts
@@ -0,0 +1,41 @@
+import {type UserDB, UserRole} from "@src/types/index.js";
+import mongoose, {type Schema, type Model} from "mongoose";
+
+const UserModelShema: Schema<UserDB> = new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+    },
+    username: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false,
+    },
+    role: {
+        type: String,
+        required: true,
+        enum: Object.values(UserRole),
+        default: UserRole.USER
+    }
+}, {
+    timestamps: true
+});
+
+UserModelShema.index({email: 1}, {unique: true});
+UserModelShema.index({username: 1}, {unique: true});
+
+const UserModel: Model<UserDB> =
+    mongoose.models.User
+    || mongoose.model<UserDB>("User", UserModelShema, "users");
+
+export {UserModelShema, UserModel};
