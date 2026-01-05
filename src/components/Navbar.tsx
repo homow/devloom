523c7550088f9963293161f070c98fd7f999c277
bitazarei <bitazarei79@gmail.com>
2026-01-06 00:21:31 +0330

    First commit on the frontend branch

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+type Props = {
+    name: string;
+  };
+  
+  const Hello: React.FC<Props> = ({ name }) => {
+    return <h1>Hello, {name}!</h1>;
+  };
+  
+  export default Hello;
