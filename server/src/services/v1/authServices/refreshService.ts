4c0a96143ce14dcf114bff53a661dd31f1f035e7
homow <homowsh897@gmail.com>
2026-01-09 12:31:33 +0330

    refactor(refreshService) with refreshTokenProvider

--- server/src/services/v1/authServices/refreshService.ts
+++ server/src/services/v1/authServices/refreshService.ts
@@ -1,85 +1,54 @@
 import {checkUserDB} from "@src/lib/index.js";
 import {createTokenAndOptions} from "@utils/tokens.js";
-import type {ServiceResponse} from "@src/types/index.js";
-import {hashSecretToken, verifyToken} from "@utils/crypto.js";
-import {createRefreshTokenService, findRefreshToken, updateRefreshToken} from "./index.js";
+import {createRefreshTokenService, updateRefreshToken} from "./index.js";
+import type {AuthPayload, RefreshToken, ServiceResponse} from "@src/types/index.js";
+import {refreshTokenProvider} from "@services/v1/authServices/refreshTokenProvider.js";
 
 export async function refreshService(
     oldToken: string
 ): Promise<ServiceResponse> {
-    if (!oldToken) {
-        return {
-            status: 401,
-            data: {
-                ok: false,
-                message: "Refresh token is missing or expired",
-                code: "REFRESH_TOKEN_NOT_FOUND",
-            }
-        };
-    }
+    const checkRefreshToken = await refreshTokenProvider(oldToken);
 
-    try {
-        const userPayload = verifyToken(oldToken);
-        const hashedOldToken: string = hashSecretToken(oldToken);
-        const session = await findRefreshToken(hashedOldToken, userPayload.id);
+    if (checkRefreshToken.status !== 200) return checkRefreshToken;
 
-        if (!session || session.isRevoked) {
-            return {
-                status: 401,
-                data: {
-                    ok: false,
-                    message: "Invalid or expired refresh token",
-                    code: "REFRESH_TOKEN_INVALID",
-                }
-            };
-        }
+    const session = checkRefreshToken.refreshTokenSession as RefreshToken;
+    const userPayload = checkRefreshToken.payload as AuthPayload;
 
-        await updateRefreshToken(session._id);
+    await updateRefreshToken(session._id);
 
-        const refreshToken = createTokenAndOptions({
-            payload: {
-                id: userPayload.id,
-                role: userPayload.role,
-                remember: userPayload.remember,
-            },
-            tokenType: "refresh",
+    const refreshToken = createTokenAndOptions({
+        payload: {
+            id: userPayload.id,
+            role: userPayload.role,
             remember: userPayload.remember,
-        });
-        const accessToken = createTokenAndOptions({
-            payload: {
-                id: userPayload.id,
-                role: userPayload.role,
-            },
-            tokenType: "access"
-        });
+        },
+        tokenType: "refresh",
+        remember: userPayload.remember,
+    });
+    const accessToken = createTokenAndOptions({
+        payload: {
+            id: userPayload.id,
+            role: userPayload.role,
+        },
+        tokenType: "access"
+    });
 
-        const expiresAt: Date = userPayload.remember
-            ? new Date(Date.now() + 7 * 24 * 60 * 60 * 1000) // 7d
-            : new Date(Date.now() + 24 * 60 * 60 * 1000);    // 1d
+    const expiresAt: Date = userPayload.remember
+        ? new Date(Date.now() + 7 * 24 * 60 * 60 * 1000) // 7d
+        : new Date(Date.now() + 24 * 60 * 60 * 1000);    // 1d
 
-        await createRefreshTokenService(userPayload.id, refreshToken.token, expiresAt);
+    await createRefreshTokenService(userPayload.id, refreshToken.token, expiresAt);
 
-        const user = await checkUserDB({id: userPayload.id});
+    const user = await checkUserDB({id: userPayload.id});
 
-        return {
-            status: 200,
-            data: {
-                ok: true,
-                message: "success",
-                accessToken: accessToken.token,
-                user
-            },
-            refreshToken,
-        };
-
-    } catch (e) {
-        return {
-            status: 401,
-            data: {
-                ok: false,
-                message: (e as Error).message || "refresh token is invalid or expired",
-                code: "INVALID_REFRESH_TOKEN",
-            }
-        };
-    }
+    return {
+        status: 200,
+        data: {
+            ok: true,
+            message: "success",
+            accessToken: accessToken.token,
+            user
+        },
+        refreshToken,
+    };
 }
