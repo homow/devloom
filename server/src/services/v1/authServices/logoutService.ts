f96e3ebd9c2d251c0e97b54071f2c5a7cfa24c90
homow <homowsh897@gmail.com>
2026-01-09 12:36:37 +0330

    feat(provider) completed logoutService

--- server/src/services/v1/authServices/logoutService.ts
+++ server/src/services/v1/authServices/logoutService.ts
@@ -1,5 +1,22 @@
-import type {ServiceResponse} from "@src/types/index.js";
+import RefreshTokenModel from "@models/RefreshToken.model.js";
+import type {RefreshToken, ServiceResponse} from "@src/types/index.js";
+import {refreshTokenProvider} from "@services/v1/authServices/refreshTokenProvider.js";
 
-export async function logoutService(): Promise<ServiceResponse> {
+export async function logoutService(
+    token: string,
+): Promise<ServiceResponse> {
+    const checkRefreshToken = await refreshTokenProvider(token);
+    if (checkRefreshToken.status !== 200) return checkRefreshToken;
 
+    const session = checkRefreshToken.refreshTokenSession as RefreshToken;
+
+    await RefreshTokenModel.findByIdAndUpdate(session._id, {$set: {isRevoked: true}});
+
+    return {
+        status: 200,
+        data: {
+            ok: true,
+            message: "Logged out successfully",
+        }
+    };
 }
