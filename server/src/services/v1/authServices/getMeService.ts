178ebb472c47fc462148b27f293316af4b6aef49
homow <homowsh897@gmail.com>
2026-01-10 10:34:16 +0330

    feat(getMe) add service for route

---
+++ server/src/services/v1/authServices/getMeService.ts
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+import {checkUserDB, getSafeUser} from "@src/lib/index.js";
+import type {AuthPayload, ServiceResponse} from "@src/types/index.js";
+
+export async function getMeService(
+    userPayload: AuthPayload
+): Promise<ServiceResponse> {
+    if (!userPayload) {
+        return {
+            status: 401,
+            data: {
+                ok: false,
+                message: "Unauthorized",
+                code: "UNAUTHORIZED",
+            }
+        };
+    }
+
+    const isValidID: boolean = mongoose.isValidObjectId(userPayload.id);
+
+    if (!isValidID) {
+        return {
+            status: 400,
+            data: {
+                ok: false,
+                message: "invalid user id",
+                code: "INVALID_ID",
+            }
+        };
+    }
+
+    const userExist = await checkUserDB({id: userPayload.id});
+
+    if (!userExist) {
+        return {
+            status: 404,
+            data: {
+                ok: false,
+                message: "user does not exists",
+                code: "USER_NOT_FOUND",
+            }
+        };
+    }
+
+    return {
+        status: 200,
+        data: {
+            ok: true,
+            message: "user successfully found",
+            user: getSafeUser(userExist)
+        }
+    };
+}
