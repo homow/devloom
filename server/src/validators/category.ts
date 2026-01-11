554d62c3f9fba48babcd02b0d5aa5623cb86e45a
homow <homowsh897@gmail.com>
2026-01-11 13:42:50 +0330

    feat(validator) add min and max length

--- server/src/validators/category.ts
+++ server/src/validators/category.ts
@@ -2,7 +2,7 @@ import z from "zod";
 
 export const CategorySchema = z.object({
     title: z.string().min(2).max(20),
-    href: z.string(),
+    href: z.string().min(2).max(20),
 });
 
 export type CategoryInput = z.infer<typeof CategorySchema>;
