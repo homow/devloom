ab382fa59dce4a17888e3d50c2941b0242fe4bb7
homow <homowsh897@gmail.com>
2026-01-11 14:02:29 +0330

    feat(category-model) add timestamp

--- server/src/models/Category.model.ts
+++ server/src/models/Category.model.ts
@@ -2,19 +2,20 @@ import type {CategoryDB} from "@src/types/index.js";
 import mongoose, {type Model, type Schema} from "mongoose";
 
 const CategoryModelShema: Schema<CategoryDB> = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-        minlength: 2,
-        maxlength: 20,
-    },
-    href: {
-        type: String,
-        required: true,
-        minlength: 2,
-        maxlength: 20,
-    }
-});
+        title: {
+            type: String,
+            required: true,
+            minlength: 2,
+            maxlength: 20,
+        },
+        href: {
+            type: String,
+            required: true,
+            minlength: 2,
+            maxlength: 20,
+        }
+    }, {timestamps: true}
+);
 
 CategoryModelShema.index({title: 1}, {unique: true});
 CategoryModelShema.index({href: 1}, {unique: true});
