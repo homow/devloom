dbe25e4e9886dcf1c0091b9c4d381418fb8c9d47
homow <homowsh897@gmail.com>
2026-01-11 11:04:02 +0330

    feat(Model) completed category Model

--- server/src/models/category.model.ts
+++ server/src/models/category.model.ts
@@ -1,5 +1,19 @@
-import mongoose, {type Model, type Models, type Schema} from "mongoose";
+import type {CategoryDB} from "@src/types/index.js";
+import mongoose, {type Model, type Schema} from "mongoose";
 
-const CategoryModelShema = new mongoose.Schema({
-
+const CategoryModelShema: Schema<CategoryDB> = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 20
+    },
+    href: {
+        type: String,
+        required: true,
+    }
 });
+
+const CategoryModel: Model<CategoryDB> = mongoose.models.Category || mongoose.model<CategoryDB>("Category", CategoryModelShema, "category");
+
+export default CategoryModel;
