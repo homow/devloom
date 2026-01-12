120884184d479a042d58882ae55617121cf4aa53
homow <homowsh897@gmail.com>
2026-01-12 22:11:18 +0330

    feat(controller) completed edit category

---
+++ server/src/controllers/v1/category/edit.ts
@@ -0,0 +1,14 @@
+import type {Response} from "express";
+import type {AuthRequest} from "@src/types/index.js";
+import type {EditCategoryInput} from "@validators/category.js";
+import {editService} from "@services/v1/categoryServices/index.js";
+
+export async function edit(
+    req: AuthRequest<{ id: string }, {}, EditCategoryInput>,
+    res: Response
+) {
+    const {id} = req.params;
+    const {title, href} = req.body;
+    const result = await editService(id, {title, href});
+    return res.status(result.status).json(result.data);
+}
