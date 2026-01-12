2be6e216e8be6b53fe25135c35dae2a6cddc636c
homow <homowsh897@gmail.com>
2026-01-12 18:22:22 +0330

    refactor(util) use util for check exist category

--- server/src/services/v1/categoryServices/createService.ts
+++ server/src/services/v1/categoryServices/createService.ts
@@ -1,34 +1,14 @@
 import CategoryModel from "@models/Category.model.js";
 import type {ServiceResponse} from "@src/types/index.js";
 import type {CategoryInput} from "@validators/category.js";
-import {categoryProjectStage, createPipelineStage} from "@src/aggregations/index.js";
+import {checkCategoryConflict} from "@controllers/v1/category/common.js";
 
 export async function createService(
     data: CategoryInput
 ): Promise<ServiceResponse> {
-    const stage = createPipelineStage({
-        stage: categoryProjectStage,
-        filter: [{title: data.title}, {href: data.href}]
-    });
-    const [categoryExist] = await CategoryModel.aggregate(stage);
+    const categoryExist = await checkCategoryConflict(data);
 
-    if (categoryExist) {
-        const messages = [];
-
-        if (data.href === categoryExist.href) messages.push("href");
-        if (data.title === categoryExist.title) messages.push("title");
-
-        const msg = `${messages[0] !== undefined ? messages[0] : ""} ${messages.length > 1 ? "and" : ""} ${messages[1] !== undefined ? messages[1] : ""}`;
-
-        return {
-            status: 409,
-            data: {
-                message: `Category Already exist: (${msg.trim()})`,
-                ok: false,
-                category: categoryExist
-            }
-        };
-    }
+    if (categoryExist) return categoryExist;
 
     const newCategory = await CategoryModel.create({
         title: data.title,
