36e3be3cab2b7843f3d33876c6fb652ed4244711
homow <homowsh897@gmail.com>
2026-01-12 19:43:30 +0330

    feat(validator) add a validator for edit category

--- server/src/validators/category.ts
+++ server/src/validators/category.ts
@@ -6,3 +6,10 @@ export const CategorySchema = z.object({
 });
 
 export type CategoryInput = z.infer<typeof CategorySchema>;
+
+export const EditCategorySchema = z.object({
+    title: z.string().min(2).max(20).optional(),
+    href: z.string().min(2).max(20).optional(),
+});
+
+export type EditCategoryInput = z.infer<typeof EditCategorySchema>;
