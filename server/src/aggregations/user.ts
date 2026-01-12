f79d7311cfdeac36ab67d9f921d340a33a03eaff
homow <homowsh897@gmail.com>
2026-01-12 15:55:25 +0330

    feat(aggregate) add a util for create Aggregate Stage

--- server/src/aggregations/user.ts
+++ server/src/aggregations/user.ts
@@ -1,30 +1,11 @@
+import {baseStage} from "./index.js";
 import {type PipelineStage} from "mongoose";
-import {createQueryPattern} from "@src/lib/index.js";
 
-export const userProjectStage = {
+export const userProjectStage: PipelineStage = {
     $project: {
-        _id: 0,
-        id: {$toString: "$_id"},
+        ...baseStage,
         name: 1,
         email: 1,
         role: 1,
-        createdAt: {$toString: "$createdAt"},
-        updatedAt: {$toString: "$updatedAt"}
     }
 };
-
-export function userAggregate(
-    filter?: Record<string, unknown>[],
-    useAnd: boolean = false
-) {
-    const stages: PipelineStage[] = [userProjectStage];
-
-    if (filter && filter.length > 0) {
-        const matchStage = createQueryPattern(
-            filter,
-            useAnd
-        );
-        stages.unshift({$match: matchStage} as PipelineStage);
-    }
-    return stages;
-}
