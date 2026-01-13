9a1d3b0ae39ea54afd4d51b60893f320c8884333
homow <homowsh897@gmail.com>
2026-01-13 17:42:31 +0330

    feat(model) completed lesson

---
+++ server/src/models/Lesson.model.ts
@@ -0,0 +1,36 @@
+import type {LessonDB} from "@src/types/index.js";
+import mongoose, {type Model, type Schema} from "mongoose";
+
+const LessonModelShema: Schema<LessonDB> = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        minlength: 3,
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    time: {
+        type: String,
+        required: true,
+        minlength: 1
+    },
+    video: {
+        type: String,
+        required: true,
+    },
+    course: {
+        type: mongoose.Types.ObjectId,
+        ref: "Course",
+    }
+}, {
+    timestamps: true
+});
+
+LessonModelShema.index({title: 1}, {unique: true});
+
+const LessonModel: Model<LessonDB> = mongoose.models.Lesson || mongoose.model<LessonDB>("Lesson", LessonModelShema, "lessons");
+
+export default LessonModel;
