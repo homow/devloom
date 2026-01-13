ec1cca69ac1799722f3965b6ec9b2856aea0dd23
homow <homowsh897@gmail.com>
2026-01-13 12:30:29 +0330

    refactor(validator) trimmed data

--- server/src/validators/category.ts
+++ server/src/validators/category.ts
@@ -3,7 +3,14 @@ import z from "zod";
 export const CategorySchema = z.object({
     title: z.string().min(2).max(20),
     href: z.string().min(2).max(20),
-});
+}).overwrite(
+    data => {
+        return {
+            title: data.title.trim(),
+            href: data.href.trim(),
+        };
+    }
+);
 
 export type CategoryInput = z.infer<typeof CategorySchema>;
 
