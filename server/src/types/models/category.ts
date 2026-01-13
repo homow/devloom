cf732013e31d08daa7db84d231ac8f9b5219c5ce
homow <homowsh897@gmail.com>
2026-01-13 11:09:27 +0330

    refactor(type) use type for return type safe

--- server/src/types/models/category.ts
+++ server/src/types/models/category.ts
@@ -1,6 +1,8 @@
-import type {BaseDB} from "./index.js";
+import type {BaseDB, SafeBaseDB} from "./index.js";
 
 export interface CategoryDB extends BaseDB {
     title: string;
     href: string;
 }
+
+export type SafeCategory = Pick<CategoryDB, "href" | "title"> & SafeBaseDB;
