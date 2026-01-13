46a9dc4ced40001bc4c8a4c5773240076ef2f968
homow <homowsh897@gmail.com>
2026-01-13 11:52:12 +0330

    feat(route) add route for edit category

--- server/src/routes/v1/category.ts
+++ server/src/routes/v1/category.ts
@@ -3,6 +3,7 @@ import {UserRole} from "@src/types/index.js";
 import * as validator from "@validators/index.js";
 import * as middleware from "@middleware/index.js";
 import * as categoryController from "@controllers/v1/category/index.js";
+import {checkRole} from "@middleware/index.js";
 
 const categoryRoute = express.Router();
 
@@ -16,5 +17,13 @@ categoryRoute
         categoryController.create
     )
     .get(categoryController.getAll);
+
+categoryRoute
+    .route("/:id")
+    .put(
+        checkRole({requiredRole: UserRole.ADMIN}),
+        middleware.isValidParamId("category"),
+        categoryController.edit
+    );
 
 export {categoryRoute};
