4c6d8e967cebba5373ef335c31c3aa0eb1eda94e
homow <homowsh897@gmail.com>
2026-01-14 19:05:59 +0330

    feat(route) add middlewares and add a route for create

--- server/src/routes/v1/course.ts
+++ server/src/routes/v1/course.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import {UserRole} from "@src/types/index.js";
+import * as middleware from "@middleware/index.js";
+import * as courseController from "@controllers/v1/course/index.js";
 
 const courseRouter = express.Router();
+courseRouter.use(
+    middleware.checkAccessToken,
+    middleware.checkBanned(),
+    middleware.checkRole({requiredRole: UserRole.ADMIN}),
+);
 
 courseRouter
     .route("/")
-    .get();
+    .post(
+        courseController.create
+    );
 
 export {courseRouter};
