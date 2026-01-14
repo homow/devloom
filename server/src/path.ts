79a73079ff2f62d29cde615708082ad3aba15ef5
homow <homowsh897@gmail.com>
2026-01-14 20:16:40 +0330

    refactor(route) change return path

--- server/src/path.ts
+++ server/src/path.ts
@@ -1,6 +1,12 @@
 import path from "node:path";
+import {fileURLToPath} from "node:url";
+
+const __filename: string = fileURLToPath(import.meta.url);
+const __dirname: string = path.dirname(__filename);
 
 // create a path form root project
 export function createPath(inputPath: string): string {
-    return path.join(process.cwd(), inputPath);
+    const isDevelopment: boolean = __dirname.slice(__dirname.length - 3) === "src";
+    const rootPath: string = path.resolve(__dirname, isDevelopment ? "../" : "./");
+    return path.join(rootPath, inputPath);
 }
