15f72090bb6911f9c0f48b366cd240ebf211ffac
homow <homowsh897@gmail.com>
2026-01-14 13:42:30 +0330

    feat(model) add timestamp

--- server/src/models/Comment.model.ts
+++ server/src/models/Comment.model.ts
@@ -1,40 +1,45 @@
 import type {CommentDB} from "@src/types/index.js";
 import mongoose, {type Model, Types, type Schema} from "mongoose";
 
-const CommentModelShema: Schema<CommentDB> = new mongoose.Schema({
-    body: {
-        type: String,
-        required: true,
-        minLength: 3,
+const CommentModelShema: Schema<CommentDB> = new mongoose.Schema(
+    {
+        body: {
+            type: String,
+            required: true,
+            minLength: 3,
+        },
+        writer: {
+            type: Types.ObjectId,
+            required: true,
+            ref: "User",
+        },
+        isConfirm: {
+            type: Boolean,
+            default: false,
+        },
+        score: {
+            type: Number,
+            default: 5,
+        },
+        course: {
+            type: Types.ObjectId,
+            required: true,
+            ref: "Course",
+        },
+        isReply: {
+            type: Boolean,
+            required: true,
+            default: false,
+        },
+        mainComment: {
+            type: Types.ObjectId,
+            default: null,
+        },
     },
-    writer: {
-        type: Types.ObjectId,
-        required: true,
-        ref: "User",
-    },
-    isConfirm: {
-        type: Boolean,
-        default: false,
-    },
-    score: {
-        type: Number,
-        default: 5,
-    },
-    course: {
-        type: Types.ObjectId,
-        required: true,
-        ref: "Course",
-    },
-    isReply: {
-        type: Boolean,
-        required: true,
-        default: false,
-    },
-    mainComment: {
-        type: Types.ObjectId,
-        default: null,
-    },
-});
+    {
+        timestamps: true,
+    }
+);
 
 CommentModelShema.index({course: 1});
 
