84600114b0dbc2fd4410004479d5a164f6263730
homow <homowsh897@gmail.com>
2026-01-14 12:24:26 +0330

    refactor(validator) overwrite and trimmed data

--- server/src/validators/lesson.ts
+++ server/src/validators/lesson.ts
@@ -7,6 +7,14 @@ export const LessonSchema = z.object({
     time: z.string().min(1),
     video: z.string().min(1),
     course: checkZodObjectId("course"),
+}).overwrite(data => {
+    return {
+        title: data.title.trim(),
+        time: data.time.trim(),
+        video: data.video.trim(),
+        free: data.free,
+        course: data.course,
+    };
 });
 
 export type LessonInput = z.infer<typeof LessonSchema>;
