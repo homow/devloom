19ad576a4da934c447e05aab42522f49d8809ca9
homow <homowsh897@gmail.com>
2026-01-14 18:40:50 +0330

    check(route) success config courses route

--- server/src/routes/v1/course.ts
+++ server/src/routes/v1/course.ts
@@ -2,4 +2,13 @@ import express from 'express';
 
 const courseRouter = express.Router();
 
+courseRouter
+    .route("/")
+    .get((_req: express.Request, res: express.Response) => {
+        return res.status(200).json({
+            ok: true,
+            message: "Welcome Back!",
+        });
+    });
+
 export {courseRouter};
