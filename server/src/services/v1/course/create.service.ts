7d431e37e728b13cba197c46792ec3a3ecda6922
homow <homowsh897@gmail.com>
2026-01-14 23:11:42 +0330

    feat(service) check course if exist

--- server/src/services/v1/course/create.service.ts
+++ server/src/services/v1/course/create.service.ts
@@ -1,5 +1,6 @@
 import type {CourseInput} from "@validators/course.js";
 import type {ServiceResponse} from "@src/types/index.js";
+import {checkCourseExist} from "./common.js";
 
 export async function createService(
     data: CourseInput,
@@ -16,4 +17,16 @@ export async function createService(
         price,
         cover
     } = data;
+
+    const courseExist = await checkCourseExist({title, href});
+
+    if (courseExist) return {
+        status: 409,
+        data: {
+            ok: false,
+            message: "course already exists",
+            code: "COURSE_EXIST",
+        }
+    };
+
 }
