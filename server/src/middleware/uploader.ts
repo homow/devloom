a1e964f8bfc5d774324d741595e1ecf1ff390642
homow <homowsh897@gmail.com>
2026-01-14 14:13:16 +0330

    feat(middleware) add a middleware for error handling and validate multiple upload files

--- server/src/middleware/uploader.ts
+++ server/src/middleware/uploader.ts
@@ -1,7 +1,13 @@
 import {multerUploader} from "@src/lib/index.js";
 import type {NextFunction, Request, Response} from "express";
 
-export function uploader(
+const fileSizeError = {
+    ok: false,
+    message: "File size too large, must be less than 3MB",
+    code: "LIMIT_FILE_SIZE"
+};
+
+export function singleUploader(
     req: Request,
     res: Response,
     next: NextFunction
@@ -9,15 +15,33 @@ export function uploader(
     multerUploader.single("file")(req, res, (err) => {
         if (err) {
             if (err.code === "LIMIT_FILE_SIZE") {
-                return res.status(400).json({
-                    ok: false,
-                    message: "File size too large, must be less than 3MB",
-                    code: "LIMIT_FILE_SIZE"
-                });
+                return res.status(400).json(fileSizeError);
             }
             return res.status(400).json({
                 ok: false,
-                message: err.message,
+                message: err?.message,
+                code: err?.code,
+            });
+        }
+
+        return next();
+    });
+}
+
+export function multipleUploader(
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
+    multerUploader.array("file")(req, res, (err) => {
+        if (err) {
+            if (err.code === "LIMIT_FILE_SIZE") {
+                return res.status(400).json(fileSizeError);
+            }
+            return res.status(400).json({
+                ok: false,
+                message: err?.message,
+                code: err?.code,
             });
         }
 
