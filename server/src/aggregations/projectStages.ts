d22060eea790b9a73a19e942bb0673d1bdc6dd23
homow <homowsh897@gmail.com>
2026-01-14 22:07:09 +0330

    feat(pipe line stage) develop for course

--- server/src/aggregations/projectStages.ts
+++ server/src/aggregations/projectStages.ts
@@ -1,19 +1,48 @@
 import {baseStage} from "./common.js";
 import type {PipelineStage} from "mongoose";
 
-export const categoryProjectStage: PipelineStage = {
+export const categoryProjectStage: PipelineStage[] = [{
     $project: {
         ...baseStage,
         title: 1,
-        href:1,
+        href: 1,
     }
-};
+}];
 
-export const userProjectStage: PipelineStage = {
+export const userProjectStage: PipelineStage[] = [{
     $project: {
         ...baseStage,
         name: 1,
         email: 1,
         role: 1,
     }
-};
+}];
+
+export const courseProjectStage: PipelineStage[] = [
+    {
+        $lookup: {
+            from: "users",
+            let: {teacher: "$teacher"},
+            pipeline: [
+                {$match: {$expr: {$eq: ["$_id", "$$teacher"]}}},
+
+            ],
+            as: "teacher"
+        }
+    },
+    {
+        $lookup: {
+            from: "category",
+            let: {category: "$category"},
+            pipeline: [
+                {$match: {$expr: {$eq: ["$_id", "$$category"]}}},
+            ],
+            as: "category"
+        }
+    },
+    {
+        $project: {
+            _id: 0
+        }
+    }
+];
