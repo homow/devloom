e8c28e58473cd646786c166597faf4951df3c1af
homow <homowsh897@gmail.com>
2026-01-14 19:19:52 +0330

    refactor(route)

--- server/src/middleware/checkAccessToken.ts
+++ server/src/middleware/checkAccessToken.ts
@@ -1,35 +1,39 @@
 import {verifyToken} from "@utils/crypto.js";
-import {checkIgnoredRoute} from "@utils/route.js";
 import type {NextFunction, Response} from "express";
 import type {AuthPayload, AuthRequest} from "@src/types/index.js";
+import {checkIgnoredRoute, type IgnoredRoutesKeys} from "@utils/index.js";
 
 export function checkAccessToken(
-    req: AuthRequest,
-    res: Response,
-    next: NextFunction
+    ignoreRoutes: IgnoredRoutesKeys[]
 ) {
-    const isIgnored: boolean = checkIgnoredRoute({method: req.method, path: req.originalUrl});
+    return (
+        req: AuthRequest,
+        res: Response,
+        next: NextFunction
+    ) => {
+        const isIgnored: boolean = checkIgnoredRoute({method: req.method, path: req.originalUrl, ignoreRoutes});
 
-    if (isIgnored) {
-        return next();
-    }
+        if (isIgnored) {
+            return next();
+        }
 
-    const token = req.cookies.accessToken || req.headers.authorization?.split(" ")[1];
+        const token = req.cookies.accessToken || req.headers.authorization?.split(" ")[1];
 
-    if (!token) return res.status(401).json({
-        ok: false,
-        message: 'Access token is required',
-        code: "ACCESS_TOKEN_MISSING",
-    });
-
-    try {
-        req.userPayload = verifyToken(token) as AuthPayload;
-        return next();
-    } catch (e) {
-        return res.status(401).json({
+        if (!token) return res.status(401).json({
             ok: false,
-            message: (e as Error).message || "Access token is invalid or expired",
-            code: "INVALID_ACCESS_TOKEN",
+            message: 'Access token is required',
+            code: "ACCESS_TOKEN_MISSING",
         });
-    }
+
+        try {
+            req.userPayload = verifyToken(token) as AuthPayload;
+            return next();
+        } catch (e) {
+            return res.status(401).json({
+                ok: false,
+                message: (e as Error).message || "Access token is invalid or expired",
+                code: "INVALID_ACCESS_TOKEN",
+            });
+        }
+    };
 }
