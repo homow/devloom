f4ce04f42022dc9c01ea0dc23135afecba792eb1
homow <homowsh897@gmail.com>
2026-01-14 12:23:23 +0330

    feat(validator) completed schema

---
+++ server/src/validators/lesson.ts
@@ -0,0 +1,12 @@
+import z from "zod";
+import {checkZodObjectId} from "@src/lib/index.js";
+
+export const LessonSchema = z.object({
+    title: z.string().min(3),
+    free: z.boolean().default(false),
+    time: z.string().min(1),
+    video: z.string().min(1),
+    course: checkZodObjectId("course"),
+});
+
+export type LessonInput = z.infer<typeof LessonSchema>;
