f274bad978a9eb52aae5b4127287a9f12346306f
homow <homowsh897@gmail.com>
2026-01-15 21:30:38 +0330

    comment(write)

--- server/src/middleware/courseCoverUploader.ts
+++ server/src/middleware/courseCoverUploader.ts
@@ -32,22 +32,26 @@ export function courseCoverUploader(
         const multerUploader = createMulter({pathDir, maxSize: MULTER_MAX_SIZE_IMAGE, limitFiles: allowedFiles});
 
         multerUploader.single(fileFieldName)(req, res, async (err) => {
-            const file = req.file;
+            const file = req.file; // save file
 
+            // check body
             const body = JSON.parse(req.body[otherDataFieldName]);
             const result = schema.safeParse(body);
 
+            // return if invalid body
             if (!result.success) return res.status(422).json({
                 ok: false,
                 errors: formatZodError(result.error),
                 message: "body is invalid"
             });
 
+            // check exist
             const courseExist = await checkCourseExist({
                 title: (result.data as CourseInput).title,
                 href: (result.data as CourseInput).href,
             });
 
+            // return and delete file if exist
             if (courseExist) {
                 fs.rmSync(`public/uploads/${pathDir}/${file?.filename}`);
                 return res.status(409).json({
@@ -58,8 +62,10 @@ export function courseCoverUploader(
                 });
             }
 
+            // save valid body
             req.body = result.data;
 
+            // error handling for multer
             if (err) {
                 if (err.code === "LIMIT_FILE_SIZE") {
                     return res.status(400).json({
@@ -75,6 +81,7 @@ export function courseCoverUploader(
                 });
             }
 
+            // add filename to body
             req.body[fileFieldName] = file?.filename;
             return next();
         });
