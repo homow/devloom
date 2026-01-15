ed324b6b55feca9e25e744bb9802bf910f6493ba
homow <homowsh897@gmail.com>
2026-01-15 14:13:10 +0330

    comment(write) create-super-admin.ts

--- server/src/scripts/create-super-admin.ts
+++ server/src/scripts/create-super-admin.ts
@@ -10,6 +10,7 @@ import {UserRole} from "@src/types/models/auth.js";
 dotenv.config({path: "../.env", quiet: true});
 const prompt = promptSync();
 
+// get prompt and validate in zod
 async function askValidated<T extends keyof typeof UserSchema.shape>(
     field: T,
     message: string,
@@ -18,7 +19,11 @@ async function askValidated<T extends keyof typeof UserSchema.shape>(
     while (true) {
         const input: string = hidden ? prompt(message, {echo: "*"}) : prompt(message);
         const parsed = UserSchema.shape[field].safeParse(input);
+
+        // return data if validation successfully
         if (parsed.success) return input;
+
+        // while validation successfully
         console.error("❌ Invalid", field, "-", parsed.error.issues[0]?.message);
         console.log("↺ Please try again.");
     }
@@ -26,10 +31,12 @@ async function askValidated<T extends keyof typeof UserSchema.shape>(
 
 async function main() {
     try {
+        // check URI in environment
         if (!process.env.MONGODB_URI) {
             console.error("MONGODB_URI is not defined in the environment");
             process.exit(1);
         }
+        // connect on db
         await mongoose.connect(process.env.MONGODB_URI);
         console.log(`Connected to DB ${process.env.MONGODB_URI}`);
 
@@ -47,8 +54,10 @@ async function main() {
         const email: string = await askValidated("email", "Enter email address: ");
         const password: string = await askValidated("password", "Enter password: ", true);
 
+        // hash password for db collection
         const hashed: string = await bcrypt.hash(password, 12);
 
+        // created SUPER_ADMIN
         const user = await UserModel.create({
             email,
             password: hashed,
@@ -63,4 +72,5 @@ async function main() {
     }
 }
 
+// run script
 await main();
