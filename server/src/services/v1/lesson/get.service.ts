e2ce16b68455f160c1e8701b52885e87da8f6981
homow <homowsh897@gmail.com>
2026-01-16 22:26:20 +0330

    feat(services) completed

--- server/src/services/v1/lesson/get.service.ts
+++ server/src/services/v1/lesson/get.service.ts
@@ -1,12 +1,42 @@
+import mongoose from "mongoose";
 import {checkObjectID} from "@src/lib/index.js";
 import LessonModel from "@models/Lesson.model.js";
 import {lessonProjectStage, createPipelineStage} from "@src/aggregations/index.js";
+import type {ServiceResponse} from "@src/types/index.js";
 
 interface CheckLessonParams {
     data?: {
         title?: string,
     };
     id?: string;
+}
+
+export async function getServices(
+    courseID: string,
+): Promise<ServiceResponse> {
+    const lessonsPipelineStages = createPipelineStage({
+        stage: lessonProjectStage,
+        filter: [{course: new mongoose.Types.ObjectId(courseID)}]
+    });
+    const lessons = await LessonModel.aggregate(lessonsPipelineStages);
+
+    if (!lessons || lessons.length === 0) return {
+        status: 404,
+        data: {
+            ok: false,
+            message: "this course no have lessons",
+            code: "NOT_FOUND_LESSONS",
+        }
+    };
+
+    return {
+        status: 200,
+        data: {
+            ok: true,
+            message: "lessons successfully found",
+            lessons,
+        }
+    };
 }
 
 export async function checkLessonExist(
