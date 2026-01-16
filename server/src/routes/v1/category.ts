bf8da6867092a71c63f0cfaf4912579c680245dc
homow <homowsh897@gmail.com>
2026-01-16 21:14:29 +0330

    refactor(ignoreRoutes) use ignoreRouteRegex util

--- server/src/routes/v1/category.ts
+++ server/src/routes/v1/category.ts
@@ -2,13 +2,16 @@ import express from "express";
 import {UserRole} from "@src/types/index.js";
 import * as validator from "@validators/index.js";
 import * as middleware from "@middleware/index.js";
-import type {IgnoredRoutesKeys} from "@utils/route.js";
 import * as categoryController from "@controllers/v1/category/index.js";
+import {getRegexForIgnoreRoutes, type IgnoredRoutesKeys} from "@utils/route.js";
 
 const categoryRoute = express.Router();
 
+const ignoreRouteGetCategory = getRegexForIgnoreRoutes("/category/([^/]+)$");
+
 const ignoreRoutes: IgnoredRoutesKeys[] = [
     {method: "GET", path: "/category"},
+    {method: "GET", path: ignoreRouteGetCategory},
 ];
 
 categoryRoute.use(middleware.checkAccessToken(ignoreRoutes), middleware.checkBanned(ignoreRoutes));
