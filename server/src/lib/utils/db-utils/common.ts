fc2533f7408ec90de92b6bb6ea1d324f9bf47a66
homow <homowsh897@gmail.com>
2026-01-16 17:55:55 +0330

    fix(pattern) fox queryPattern when has _id key

--- server/src/lib/utils/db-utils/common.ts
+++ server/src/lib/utils/db-utils/common.ts
@@ -1,8 +1,18 @@
+import mongoose from "mongoose";
+
 export function createQueryPattern(
     params: Record<string, unknown>[],
     useAnd?: boolean
 ) {
-    const filtered: Record<string, unknown>[] = params.filter(v => Object.values(v)[0] !== undefined);
+    const filtered: Record<string, unknown>[] = params.filter(v => Object.values(v)[0] !== undefined).map(v => {
+        if (Object.keys(v)[0] === "_id") {
+            return {
+                _id: new mongoose.Types.ObjectId(Object.values(v)[0] as string)
+            };
+        }
+
+        return v;
+    });
 
     if (filtered.length === 0) throw new Error("At least one condition must be provided");
     if (filtered.length === 1) return filtered[0];
