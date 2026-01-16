edf11c8a1f5289db7d384455a1f6f8e967356423
homow <homowsh897@gmail.com>
2026-01-16 17:00:44 +0330

    fix(validation) delete course

--- server/src/validators/lesson.ts
+++ server/src/validators/lesson.ts
@@ -1,19 +1,16 @@
 import z from "zod";
-import {checkZodObjectId} from "@src/lib/index.js";
 
 export const LessonSchema = z.object({
     title: z.string().min(3),
     free: z.boolean().default(false),
     time: z.string().min(1),
     video: z.string().min(1),
-    course: checkZodObjectId("course"),
 }).overwrite(data => {
     return {
         title: data.title.trim(),
         time: data.time.trim(),
         video: data.video.trim(),
         free: data.free,
-        course: data.course,
     };
 });
 
