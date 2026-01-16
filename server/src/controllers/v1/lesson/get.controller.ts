54c23e5e55ec6c8e828f728381c19197ac8ea179
homow <homowsh897@gmail.com>
2026-01-16 19:51:10 +0330

    feat(controller) completed controller for get a lesson

--- server/src/controllers/v1/lesson/get.controller.ts
+++ server/src/controllers/v1/lesson/get.controller.ts
@@ -1,8 +1,40 @@
 import type {Request, Response} from "express";
+import mongoose from "mongoose";
+import {checkLessonExist} from "@services/v1/lesson/index.js";
 
 export async function get(
-    req: Request<{course?: string, }>,
+    req: Request<{ courseID?: string, lessonID?: string }>,
     res: Response
 ) {
+    const courseID = req.params.courseID;
+    const lessonID = req.params.lessonID;
 
+    if (courseID && lessonID) {
+        const isValidCourseID: boolean = mongoose.isValidObjectId(courseID);
+        const isValidLessonID: boolean = mongoose.isValidObjectId(lesson);
+
+        if (!isValidLessonID || !isValidCourseID) return res.status(400).json({
+            ok: false,
+            message: "invalid ID",
+            code: "INVALID_ID",
+            errors: [
+                !isValidCourseID ? "invalid course id" : undefined,
+                !isValidLessonID ? "invalid lesson id" : undefined,
+            ]
+        });
+
+        const lesson = await checkLessonExist({id: lessonID});
+
+        if (!lesson) return res.status(404).json({
+            ok: false,
+            message: "lesson not found",
+            code: "NOT_EXIST_LESSON",
+        });
+
+        res.status(200).json({
+            ok: true,
+            lesson,
+            message: "lesson successfully found",
+        });
+    }
 }
