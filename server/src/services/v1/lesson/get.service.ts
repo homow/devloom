f9ec0dd434d5b913313adc891b5d71b4b447d800
homow <homowsh897@gmail.com>
2026-01-16 19:04:28 +0330

    feat(service) add service for getLessons

---
+++ server/src/services/v1/lesson/get.service.ts
@@ -0,0 +1,44 @@
+import {checkObjectID} from "@src/lib/index.js";
+import LessonModel from "@models/Lesson.model.js";
+import {lessonProjectStage, createPipelineStage} from "@src/aggregations/index.js";
+
+interface CheckLessonParams {
+    data?: {
+        title: string,
+    };
+    id?: string;
+}
+
+export async function checkLessonExist(
+    {
+        data,
+        id
+    }: CheckLessonParams
+) {
+    if (id) {
+        const checkInvalidID = checkObjectID(id);
+        if (checkInvalidID) return checkInvalidID;
+    }
+
+    if (
+        id === undefined
+        && data?.title === undefined
+    ) {
+        const lessonStage = createPipelineStage({
+            stage: lessonProjectStage,
+        });
+
+        const [lessonExist] = await LessonModel.aggregate(lessonStage);
+        if (lessonExist) return lessonExist;
+    }
+
+    const filterLessonStage = createPipelineStage({
+        stage: lessonProjectStage,
+        filter: [{title: data?.title}, {_id: id}]
+    });
+
+    const [lessonExist] = await LessonModel.aggregate(filterLessonStage);
+    if (lessonExist) return lessonExist;
+
+    return null;
+}
