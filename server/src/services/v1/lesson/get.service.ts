1a2128c0fedd1d6ff15c54db14a685bedd79782e
homow <homowsh897@gmail.com>
2026-01-17 16:25:40 +0330

    refactor(service)

--- server/src/services/v1/lesson/get.service.ts
+++ server/src/services/v1/lesson/get.service.ts
@@ -1,40 +1,7 @@
 import mongoose from "mongoose";
 import {checkObjectID} from "@src/lib/index.js";
 import LessonModel from "@models/Lesson.model.js";
-import type {ServiceResponse} from "@src/types/index.js";
 import {lessonProjectStage, createPipelineStage} from "@src/aggregations/index.js";
-
-/** get all lessons from one course */
-export async function getServices(
-    courseID: string,
-): Promise<ServiceResponse> {
-    /** find all lessons from course with Pipeline */
-    const lessonsPipelineStages = createPipelineStage({
-        stage: lessonProjectStage,
-        filter: [{course: new mongoose.Types.ObjectId(courseID)}]
-    });
-    const lessons = await LessonModel.aggregate(lessonsPipelineStages);
-
-    /** if not found lesson */
-    if (!lessons || lessons.length === 0) return {
-        status: 404,
-        data: {
-            ok: false,
-            message: "this course no have lessons",
-            code: "NOT_FOUND_LESSONS",
-        }
-    };
-
-    /** find lessons and return response */
-    return {
-        status: 200,
-        data: {
-            ok: true,
-            message: "lessons successfully found",
-            lessons,
-        }
-    };
-}
 
 interface CheckLessonParams {
     data?: {
@@ -75,7 +42,7 @@ export async function checkLessonExist(
         filter: [{title: data?.title}, {course: new mongoose.Types.ObjectId(data?.course)}, {_id: id}]
     });
 
-    const [lessonExist] = await LessonModel.aggregate(filterLessonStage);
+    const lessonExist = await LessonModel.aggregate(filterLessonStage);
     if (lessonExist) return lessonExist;
 
     return null;
