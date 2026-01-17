03dd028e951343a43a2330477bb636f69cc56366
homow <homowsh897@gmail.com>
2026-01-17 23:15:47 +0330

    comment(write)

--- server/src/services/v1/lesson/create.service.ts
+++ server/src/services/v1/lesson/create.service.ts
@@ -1,15 +1,18 @@
 import {checkLessonExist} from "./index.js";
 import LessonModel from "@models/Lesson.model.js";
 import type {LessonInput} from "@validators/lesson.js";
-import type {SafeLessonDB, ServiceResponse} from "@src/types/index.js";
 import {checkCourseExist} from "@services/v1/course/common.js";
+import type {SafeLessonDB, ServiceResponse} from "@src/types/index.js";
 
+/** create lesson for a course */
 export async function createService(
     id: string,
     data: LessonInput
 ): Promise<ServiceResponse> {
+    /** check exist course */
     const existCourse = await checkCourseExist({id});
 
+    /** return if not exist course */
     if (!existCourse) return {
         status: 404,
         data: {
@@ -21,8 +24,10 @@ export async function createService(
 
     const {title, free, video, time} = data;
 
+    /** check exist lesson with title */
     const lessonExist = await checkLessonExist({data: {title}});
 
+    /** return if lesson exist */
     if ((lessonExist as SafeLessonDB[])[0]) return {
         status: 409,
         data: {
@@ -33,6 +38,7 @@ export async function createService(
         }
     };
 
+    /** create new lesson */
     const newLesson = await LessonModel.create({
         title,
         time,
@@ -41,8 +47,10 @@ export async function createService(
         course: id,
     });
 
+    /** get lesson with PipelineStage and join all reference */
     const lesson = await checkLessonExist({id: newLesson._id});
 
+    /** return successfully response and new lesson */
     return {
         status: 201,
         data: {
