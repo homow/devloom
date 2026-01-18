b40656f65c97809271f19b3833966c907d577677
homow <homowsh897@gmail.com>
2026-01-18 18:21:53 +0330

    feat(services) completed lesson delete

---
+++ server/src/services/v1/lesson/delete.service.ts
@@ -0,0 +1,25 @@
+import LessonModel from "@models/Lesson.model.js";
+import type {ServiceResponse} from "@src/types/index.js";
+
+export async function deleteService(
+    id: string
+): Promise<ServiceResponse> {
+    const lessonDeleted = await LessonModel.findByIdAndDelete(id);
+
+    if (!lessonDeleted) return {
+        status: 404,
+        data: {
+             ok: false,
+            message: "lesson not found",
+            code: "NOT_FOUND",
+        }
+    };
+
+    return {
+        status: 200,
+        data: {
+            ok: true,
+            message: "lesson successfully deleted",
+        }
+    };
+}
