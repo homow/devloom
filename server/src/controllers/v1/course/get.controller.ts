4a516375dd3645bcb23ada0dd41be90d5cd2e703
homow <homowsh897@gmail.com>
2026-01-18 17:26:50 +0330

    feat(controller) completed get all or one course

--- server/src/controllers/v1/course/get.controller.ts
+++ server/src/controllers/v1/course/get.controller.ts
@@ -1,14 +1,27 @@
 import type {Request, Response} from "express";
 import {checkCourseExist} from "@services/v1/course/index.js";
 
+/** get all or one course from services and return to client */
 export async function get(
-    req: Request,
+    req: Request<{ href?: string }>,
     res: Response
 ) {
-    const courses = await checkCourseExist();
-    return res.status(200).json({
+    const courses = await checkCourseExist({data: {href: req.params.href}});
+
+    const response = {
         ok: true,
-        message: "courses successfully found.",
-        courses
-    });
+        message: "",
+        courses: undefined,
+        course: undefined
+    };
+
+    if (req.params.href) {
+        response.message = "course successfully found.";
+        response.course = courses;
+    } else {
+        response.message = "all courses successfully found.";
+        response.courses = courses;
+    }
+
+    return res.status(200).json(response);
 }
