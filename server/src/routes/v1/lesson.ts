ab3865be1d743e18476d82d51c035b7503c76f6b
homow <homowsh897@gmail.com>
2026-01-18 18:48:50 +0330

    feat(ignoreRoute) add ignoreRouteRegex for get one lesson

--- server/src/routes/v1/lesson.ts
+++ server/src/routes/v1/lesson.ts
@@ -2,14 +2,24 @@ import express from "express";
 import {UserRole} from "@src/types/index.js";
 import * as middleware from "@middleware/index.js";
 import * as lessonController from "@controllers/v1/lesson/index.js";
+import {getRegexForIgnoreRoutes, type IgnoredRoutesKeys} from "@utils/route.js";
 
 const lessonRouter = express.Router();
 
+const getOneLessonRegex: RegExp = getRegexForIgnoreRoutes("/course/([^/]+)/lesson/([^/]+)$");
+
+/** ignore this route in protected route */
+const ignoreRoutes: IgnoredRoutesKeys[] = [
+    {method: "GET", path: getOneLessonRegex},
+];
+
+/** set global middleware */
 lessonRouter.use(
-    middleware.checkAccessToken(),
-    middleware.checkBanned()
+    middleware.checkAccessToken(ignoreRoutes),
+    middleware.checkBanned(ignoreRoutes)
 );
 
+/** delete and get one lesson */
 lessonRouter
     .route("/:id")
     .get(
