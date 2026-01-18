7f17c9bb4400fd47e67d131836cc03c645dbfb5b
homow <homowsh897@gmail.com>
2026-01-18 17:53:31 +0330

    feat(controller) completed delete course controller

---
+++ server/src/controllers/v1/course/delete.controller.ts
@@ -0,0 +1,12 @@
+import type {Response} from "express";
+import type {AuthRequest} from "@src/types/index.js";
+import {deleteService} from "@services/v1/course/index.js";
+
+export async function deleteController(
+    req: AuthRequest<{ id: string }>,
+    res: Response
+) {
+    const {id} = req.params;
+    const result = await deleteService(id);
+    res.status(result.status).json(result.data);
+}
