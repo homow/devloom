1a21fbd9872ed39362808f8ad14c0bb854a5df8d
homow <homowsh897@gmail.com>
2026-01-18 18:24:05 +0330

    comment(write)

--- server/src/services/v1/lesson/delete.service.ts
+++ server/src/services/v1/lesson/delete.service.ts
@@ -1,11 +1,13 @@
 import LessonModel from "@models/Lesson.model.js";
 import type {ServiceResponse} from "@src/types/index.js";
 
+/** find and delete on lesson */
 export async function deleteService(
     id: string
 ): Promise<ServiceResponse> {
     const lessonDeleted = await LessonModel.findByIdAndDelete(id);
 
+    /** if lesson not found */
     if (!lessonDeleted) return {
         status: 404,
         data: {
