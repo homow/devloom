dd9f8ec2125c196cd01b341599007ef0feaeff78
homow <homowsh897@gmail.com>
2026-01-18 18:33:41 +0330

    feat(route) add a new file route for lessons

---
+++ server/src/routes/v1/lesson.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import {UserRole} from "@src/types/index.js";
+import * as middleware from "@middleware/index.js";
+import * as lessonController from "@controllers/v1/lesson/index.js";
+
+const lessonRouter = express.Router();
+
+lessonRouter
+    .route("/:id")
+    .delete(
+        middleware.checkRole({requiredRole: UserRole.ADMIN}),
+        middleware.isValidParamId("lesson"),
+        lessonController.deleteController
+    );
