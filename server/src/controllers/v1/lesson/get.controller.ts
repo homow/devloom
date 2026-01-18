9fc100b55eab6966e534539e111be7d0f75ae38f
homow <homowsh897@gmail.com>
2026-01-18 18:43:30 +0330

    refactor(controller)

--- server/src/controllers/v1/lesson/get.controller.ts
+++ server/src/controllers/v1/lesson/get.controller.ts
@@ -5,58 +5,63 @@ import type {LessonDB, ServiceResponse} from "@src/types/index.js";
 
 /** get one lesson with id or get all lesson with course id */
 export async function get(
-    req: Request<{ courseHref: string; lessonID?: string }>,
+    req: Request<{ courseHref?: string; lessonID?: string }>,
     res: Response
 ) {
-    const courseHref: string = req.params.courseHref;
+    const courseHref: string | undefined = req.params.courseHref;
     const lessonID: string | undefined = req.params.lessonID;
 
-    /** check course on db collection */
-    const existCourse = await checkCourseExist({data: {href: courseHref}});
+    /** if courseHref in params */
+    if (courseHref) {
+        /** check course on db collection */
+        const existCourse = await checkCourseExist({data: {href: courseHref}});
 
-    if (!existCourse) return res.status(404).json({
-        ok: false,
-        message: "Course not found",
-        code: "NOT_EXIST_COURSE",
-    });
-
-    /** if it has lesson id */
-    if (lessonID) {
-        /** get one lesson from aggregate PipelineStage[] */
-        const lesson = await checkLessonExist({id: lessonID});
-
-        /** if not exist lesson */
-        if (!lesson || lesson.length === 0) return res.status(404).json({
+        /** if not exist course */
+        if (!existCourse) return res.status(404).json({
             ok: false,
-            message: "lesson not found",
-            code: "NOT_EXIST_LESSON",
+            message: "Course not found",
+            code: "NOT_EXIST_COURSE",
         });
 
-        /** if invalid id */
-        if ((lesson as ServiceResponse).status === 400) return res.status((lesson as ServiceResponse).status).json((lesson as ServiceResponse).data);
+        /** get all lessons from one course */
+        const lessons = await checkLessonExist({data: {course: existCourse.id}});
 
-        /** find and return lesson */
+        /** if not exist lessons */
+        if (!lessons || lessons.length === 0) return res.status(404).json({
+            ok: false,
+            message: "lessons not found",
+            code: "NOT_EXIST_LESSONS",
+        });
+
+        /** return lessons in response */
         return res.status(200).json({
             ok: true,
-            message: "lesson successfully found",
-            lesson: (lesson as LessonDB[])[0],
+            message: "lessons successfully found",
+            lessons,
         });
     }
 
-    /** get all lessons from one course */
-    const lessons = await checkLessonExist({data: {course: existCourse.id}});
+    /**
+     * if lesson id in params
+     *
+     * get one lesson from aggregate PipelineStage[]
+     */
+    const lesson = await checkLessonExist({id: lessonID});
 
-    /** if not exist lessons */
-    if (!lessons || lessons.length === 0) return res.status(404).json({
+    /** if not exist lesson */
+    if (!lesson || lesson.length === 0) return res.status(404).json({
         ok: false,
-        message: "lessons not found",
-        code: "NOT_EXIST_LESSONS",
+        message: "lesson not found",
+        code: "NOT_EXIST_LESSON",
     });
 
-    /** return lessons in response */
+    /** if invalid id */
+    if ((lesson as ServiceResponse).status === 400) return res.status((lesson as ServiceResponse).status).json((lesson as ServiceResponse).data);
+
+    /** find and return lesson */
     return res.status(200).json({
         ok: true,
-        message: "lessons successfully found",
-        lessons,
+        message: "lesson successfully found",
+        lesson: (lesson as LessonDB[])[0],
     });
 }
