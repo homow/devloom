e6de65c258b03ded7edaa19865a7f3cd726750b9
homow <homowsh897@gmail.com>
2026-01-19 14:28:59 +0330

    feat(swagger) completed config

--- server/src/lib/configs/swagger.ts
+++ server/src/lib/configs/swagger.ts
@@ -1,2 +1,20 @@
+import type {Express} from "express";
 import swaggerJsDoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
+
+const configSwagger = (app: Express) => {
+    const swaggerDocument = swaggerJsDoc({
+        swaggerDefinition: {
+            info: {
+                title: "Devloom - Programming Education",
+                description: "Devloom is a modular, scalable API designed for building a modern programming education platform.",
+                version: "1.0.0",
+            }
+        },
+    });
+
+    const swaggerUiDoc = swaggerUi.serveFiles(swaggerDocument);
+    app.use("/api-docs", swaggerUi.serve, swaggerUiDoc);
+};
+
+export {configSwagger};
