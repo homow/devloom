d261a56d127c7a81bb8b5a70a3b04a8c86a16efe
homow <homowsh897@gmail.com>
2026-01-19 17:23:26 +0330

    feat(swagger) move from json to yaml

--- server/src/routes/v1/swagger.ts
+++ server/src/routes/v1/swagger.ts
@@ -1,6 +1,8 @@
+import yaml from "js-yaml";
 import express from "express";
-import swaggerJson from "@docs/swagger.json" with {type: "json"};
+import {createPath} from "@src/path.js";
 import swaggerUi, {type SwaggerUiOptions} from "swagger-ui-express";
+import fs from "node:fs";
 
 const swaggerRouter = express.Router();
 
@@ -8,7 +10,12 @@ const swaggerOptions: SwaggerUiOptions = {
     customCssUrl: "/static/css/style.css",
 };
 
+const swaggerYamlPath: string = createPath("src/docs/swagger.yaml");
+
+const swaggerYamlContent = fs.readFileSync(swaggerYamlPath, "utf8");
+const swaggerDocs = yaml.load(swaggerYamlContent) as object;
+
 swaggerRouter.use("/", swaggerUi.serve);
-swaggerRouter.get("/", swaggerUi.setup(swaggerJson, swaggerOptions));
+swaggerRouter.get("/", swaggerUi.setup(swaggerDocs, swaggerOptions));
 
 export {swaggerRouter};
