efd15fb254226199ee3f4bc3b86a68c216713212
homow <homowsh897@gmail.com>
2026-01-19 16:19:48 +0330

    feat(configs)

--- server/src/routes/v1/swagger.ts
+++ server/src/routes/v1/swagger.ts
@@ -1,27 +1,14 @@
 import express from "express";
-import swaggerJsDoc from "swagger-jsdoc";
-import swaggerUi from "swagger-ui-express";
+import swaggerJson from "@docs/swagger.json" with {type: "json"};
+import swaggerUi, {type SwaggerUiOptions} from "swagger-ui-express";
 
 const swaggerRouter = express.Router();
 
-const swaggerDocument = swaggerJsDoc({
-    apis: [
-        "/src/app.ts",
-    ],
-    swaggerDefinition: {
-        info: {
-            title: "Devloom - Programming Education",
-            description: "Devloom is a modular, scalable API designed for building a modern programming education platform.",
-            version: "1.0.0",
-        },
-    },
-});
-
-const swaggerOptions = {};
-
-const swaggerUiDoc = swaggerUi.setup(swaggerDocument, {
+const swaggerOptions: SwaggerUiOptions = {
     customCssUrl: "/static/css/style.css",
-});
+};
 
-swaggerRouter.use("/api-docs", swaggerUi.serve, swaggerUiDoc);
-swaggerRouter.get(swaggerUiDoc);
+swaggerRouter.use("/", swaggerUi.serve);
+swaggerRouter.get("/", swaggerUi.setup(swaggerJson, swaggerOptions));
+
+export {swaggerRouter};
