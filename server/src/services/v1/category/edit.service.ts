e022fe21a790970a5523f6f70cb8604b34a155f9
homow <homowsh897@gmail.com>
2026-01-24 18:21:06 +0330

    refactor(services) add new response for edit category

--- server/src/services/v1/category/edit.service.ts
+++ server/src/services/v1/category/edit.service.ts
@@ -3,46 +3,95 @@ import CategoryModel from "@models/Category.model.js";
 import type {ServiceResponse} from "@src/types/index.js";
 import type {EditCategoryInput} from "@validators/category.js";
 
+interface CustomError extends Error {
+    errorResponse: {
+        ok: number;
+        code: number;
+        codeName: string;
+        keyPattern: { title: number, href: number };
+        keyValue: object;
+    }
+}
+
 export async function editService(
     id: string,
     data: EditCategoryInput
 ): Promise<ServiceResponse> {
-    const categoryExist = await CategoryModel.findById(id).lean();
+    try {
+        const categoryExist = await CategoryModel.findById(id).lean();
 
-    if (!categoryExist) return {
-        status: 404,
-        data: {
-            ok: false,
-            message: "category not found",
-            code: "NOT_FOUND",
+        if (!categoryExist) return {
+            status: 404,
+            data: {
+                ok: false,
+                message: "category not found",
+                code: "NOT_FOUND",
+            }
+        };
+
+        const newData: EditCategoryInput = {};
+
+        if (data.href) newData.href = data.href;
+        if (data.title) newData.title = data.title;
+
+        if (data.href === categoryExist.href || data.title === categoryExist.title) return {
+            status: 409,
+            data: {
+                ok: false,
+                message: "new data must be different with this category data",
+                code: "CONFLICT_DATA",
+                category: getSafeCategory(categoryExist)
+            }
+        };
+
+        const updateCategory = await CategoryModel.findByIdAndUpdate(id, {
+            $set: newData
+        }, {new: true});
+
+        return {
+            status: 200,
+            data: {
+                ok: true,
+                message: "Successfully updated category",
+                category: updateCategory && getSafeCategory(updateCategory),
+            }
+        };
+    } catch (e) {
+        const customError = e as CustomError;
+
+        if (customError.errorResponse.code === 11000) {
+            const errors = [];
+
+            if (customError.errorResponse.keyPattern.href) {
+                errors.push({
+                    field: "href",
+                    message: "href already exists in database",
+                });
+            }
+
+            if (customError.errorResponse.keyPattern.title) {
+                errors.push({
+                    field: "title",
+                    message: "title already exists in database",
+                });
+            }
+
+            return {
+                status: 409,
+                data: {
+                    ok: false,
+                    message: "data already exists in database",
+                    errors
+                }
+            };
         }
-    };
 
-    const newData: EditCategoryInput = {};
-
-    if (data.href) newData.href = data.href;
-    if (data.title) newData.title = data.title;
-
-    if (data.href === categoryExist.href || data.title === categoryExist.title) return {
-        status: 409,
-        data: {
-            ok: false,
-            message: "new data must be different with this category data",
-            code: "CONFLICT_DATA",
-            category: getSafeCategory(categoryExist)
-        }
-    };
-
-    const updateCategory = await CategoryModel.findByIdAndUpdate(id, {
-        $set: newData
-    }, {new: true});
-
-    return {
-        status: 200,
-        data: {
-            ok: true,
-            message: "Successfully updated category",
-            category: updateCategory && getSafeCategory(updateCategory),
-        }
-    };
+        return {
+            status: 500,
+            data: {
+                ok: false,
+                message: (e as Error).message || "Something went wrong, please try again later.",
+            }
+        };
+    }
 }
