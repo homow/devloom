0ce755c3bcef3931f728df383d905dbb59301882
homow <homowsh897@gmail.com>
2026-01-28 21:41:13 +0330

    feat(route) set global middlewares to comment router

--- server/src/routes/v1/comment.ts
+++ server/src/routes/v1/comment.ts
@@ -1,5 +1,17 @@
 import express from "express";
+import * as middleware from "@middleware/index.js";
+import type {IgnoredRoutesKeys} from "@utils/route.js";
 
 const commentRouter = express.Router();
+
+const ignoredRoutes: IgnoredRoutesKeys[] = [
+    {method: 'GET', path: '/comment'}
+];
+
+commentRouter.use(middleware.checkAccessToken(ignoredRoutes), middleware.checkBanned(ignoredRoutes));
+
+commentRouter
+    .route("/")
+    .post();
 
 export {commentRouter};
