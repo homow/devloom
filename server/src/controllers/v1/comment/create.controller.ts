91be8717fc52844f6fae15989e0991116d874e7a
homow <homowsh897@gmail.com>
2026-01-29 20:48:25 +0330

    comment(write) controller

--- server/src/controllers/v1/comment/create.controller.ts
+++ server/src/controllers/v1/comment/create.controller.ts
@@ -3,10 +3,12 @@ import type {AuthRequest} from "@src/types/index.js";
 import type {CommentInput} from "@validators/comment.js";
 import {createService} from "@services/v1/comment/index.js";
 
+/** created one comment */
 export async function create(
     req: AuthRequest<{}, {}, CommentInput>,
     res: Response
 ) {
+    /** get safe data */
     const {writer, course, isReply, score, parentComment, body} = req.body;
     const result = await createService({writer, course, isReply, score, parentComment, body});
     return res.status(result.status).json(result.data);
