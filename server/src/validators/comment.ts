426974e02535f463a451ce406d9738e3ae262a8d
homow <homowsh897@gmail.com>
2026-01-29 22:22:08 +0330

    refactor(schema)

--- server/src/validators/comment.ts
+++ server/src/validators/comment.ts
@@ -3,7 +3,6 @@ import {checkZodObjectId} from "@src/lib/index.js";
 
 export const CommentSchema = z.object({
     body: z.string().min(3),
-    writer: checkZodObjectId("user"),
     score: z.number().min(0).max(5).optional().default(5),
     course: checkZodObjectId("course"),
     isReply: z.boolean().optional().default(false),
@@ -11,7 +10,6 @@ export const CommentSchema = z.object({
 }).overwrite(data => {
     return {
         body: data.body.trim(),
-        writer: data.writer,
         score: data.score,
         course: data.course,
         isReply: data.isReply,
