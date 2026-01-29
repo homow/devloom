5feff400572f9b215aa3d3efbb56a18072b6115d
homow <homowsh897@gmail.com>
2026-01-29 20:59:18 +0330

    feat(service) add new response and populated

--- server/src/services/v1/comment/create.service.ts
+++ server/src/services/v1/comment/create.service.ts
@@ -1,28 +1,42 @@
 import CommentModel from "@models/Comment.model.js";
 import type {CommentInput} from "@validators/comment.js";
-import type {CommentDB, ServiceResponse} from "@src/types/index.js";
+import type {ServiceResponse} from "@src/types/index.js";
 
+/** inject comment in db */
 export async function createService(
     commentData: CommentInput
 ): Promise<ServiceResponse> {
-    const newComment = await CommentModel.create(commentData) as CommentDB;
+    const newComment = await CommentModel.create(commentData);
+
+    if (newComment) {
+        /** populated data */
+        const safeComment = await newComment.populate(['writer', 'course', 'parentComment']);
+
+        return {
+            status: 201,
+            data: {
+                ok: true,
+                message: "comment created successfully",
+                comment: {
+                    ...newComment,
+                    _id: undefined,
+                    __v: undefined,
+                    id: newComment._id.toString(),
+                    course: newComment.course.toString(),
+                    writer: newComment.writer.toString(),
+                    parentComment: newComment.parentComment ? newComment.parentComment?.toString() : undefined,
+                    createdAt: newComment.createdAt.toISOString(),
+                    updatedAt: newComment.updatedAt.toISOString(),
+                },
+            }
+        };
+    }
 
     return {
-        status: 201,
+        status: 500,
         data: {
-            ok: true,
-            message: "comment created successfully",
-            comment: {
-                ...newComment,
-                _id: undefined,
-                __v: undefined,
-                id: newComment._id.toString(),
-                course: newComment.course.toString(),
-                writer: newComment.writer.toString(),
-                parentComment: newComment.parentComment ? newComment.parentComment?.toString() : undefined,
-                createdAt: newComment.createdAt.toISOString(),
-                updatedAt: newComment.updatedAt.toISOString(),
-            },
+            ok: false,
+            message: "Internal Server Error",
         }
     };
 }
