263e85bfc68eb365f131f9b137c2d843c9a3c3c4
homow <homowsh897@gmail.com>
2026-01-29 22:49:39 +0330

    refactor(schema)

--- server/src/services/v1/comment/create.service.ts
+++ server/src/services/v1/comment/create.service.ts
@@ -1,16 +1,35 @@
 import CommentModel from "@models/Comment.model.js";
 import type {CommentInput} from "@validators/comment.js";
 import type {ServiceResponse} from "@src/types/index.js";
+import {commentProjectStage, commentWithParentStage, createPipelineStage} from "@src/aggregations/index.js";
+import {checkCourseExist} from "@services/v1/course/index.js";
 
 /** inject comment in db */
 export async function createService(
-    commentData: CommentInput
+    commentData: CommentInput & { writer: string },
 ): Promise<ServiceResponse> {
+    const course = await checkCourseExist({id: commentData.course});
+
+    if (!course) return {
+        status: 404,
+        data: {
+            ok: false,
+            message: "course not found",
+            code: "COURSE_NOT_FOUND",
+        }
+    };
+
     const newComment = await CommentModel.create(commentData);
 
     if (newComment) {
-        /** populated data */
-        const safeComment = await newComment.populate(['writer', 'course', 'parentComment']);
+        const commentStage = createPipelineStage({
+            filter: [{_id: newComment._id}],
+            stage: newComment.isReply ? commentWithParentStage : commentProjectStage,
+        });
+
+        const comment = await CommentModel.aggregate(commentStage);
+
+        console.log(comment);
 
         return {
             status: 201,
@@ -28,6 +47,7 @@ export async function createService(
                     createdAt: newComment.createdAt.toISOString(),
                     updatedAt: newComment.updatedAt.toISOString(),
                 },
+                pipeline: comment
             }
         };
     }
