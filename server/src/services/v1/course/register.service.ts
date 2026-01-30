d1ee73533374e047bb4ec0132496d56418acb988
homow <homowsh897@gmail.com>
2026-01-30 13:15:34 +0330

    feat(services) check course and add response(404)

--- server/src/services/v1/course/register.service.ts
+++ server/src/services/v1/course/register.service.ts
@@ -1,8 +1,21 @@
-import type {AuthPayload, ServiceResponse} from "@src/types/index.js";
+import {checkCourseExist} from "@services/v1/course/get.service.js";
+import type {AuthPayload ,CoursePopulate, ServiceResponse} from "@src/types/index.js";
 
 export async function registerService(
     userPayload: AuthPayload,
-    data
+    courseID: string,
 ): Promise<ServiceResponse> {
+    const courseExist = await checkCourseExist({id: courseID});
+
+    if (!courseExist) return {
+        status: 404,
+        data: {
+            ok: false,
+            message: "Course not exists",
+            code: "NOT_FOUND",
+        }
+    };
+
+    const course = courseExist as CoursePopulate;
 
 }
