060128d1b4f313c767d62492182d5bb01706ece0
homow <homowsh897@gmail.com>
2026-01-30 13:20:59 +0330

    feat(services) register in course and add response(201)

--- server/src/services/v1/course/register.service.ts
+++ server/src/services/v1/course/register.service.ts
@@ -1,5 +1,6 @@
+import CourseUserModel from "@models/course-user.model.js";
 import {checkCourseExist} from "@services/v1/course/get.service.js";
-import type {AuthPayload ,CoursePopulate, ServiceResponse} from "@src/types/index.js";
+import type {AuthPayload, CoursePopulate, ServiceResponse} from "@src/types/index.js";
 
 export async function registerService(
     userPayload: AuthPayload,
@@ -18,4 +19,29 @@ export async function registerService(
 
     const course = courseExist as CoursePopulate;
 
+    const courseUserRegister = await CourseUserModel.create({
+        course: course._id,
+        user: userPayload.id,
+        price: course.price,
+    });
+
+    return {
+        status: 201,
+        data: {
+            ok: true,
+            message: "Course Register Success",
+            user: {
+                id: userPayload.id,
+                role: userPayload.role
+            },
+            course,
+            registerData: {
+                id: courseUserRegister.id,
+                user: userPayload.user.toString(),
+                course: courseUserRegister.course.toString(),
+                createdAt: courseUserRegister.createdAt.toISOString(),
+                updatedAt: courseUserRegister.updatedAt.toISOString(),
+            },
+        }
+    };
 }
