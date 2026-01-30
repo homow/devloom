9d8ff0554fef8ad27d150bd29ba4f156ccc25deb
homow <homowsh897@gmail.com>
2026-01-30 13:12:30 +0330

    remove(validator)

--- server/src/validators/course-user.ts
+++
@@ -1,8 +0,0 @@
-import z from "zod";
-import {checkZodObjectId} from "@src/lib/index.js";
-
-export const CourseUserSchema = z.object({
-    course: checkZodObjectId("course"),
-});
-
-export type CourseUserInput = z.infer<typeof checkZodObjectId>;
