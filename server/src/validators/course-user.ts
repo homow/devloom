c6edfc9478714b2347bc9a891dd96a07b12fa56f
homow <homowsh897@gmail.com>
2026-01-30 13:10:32 +0330

    fix(validator)

--- server/src/validators/course-user.ts
+++ server/src/validators/course-user.ts
@@ -3,8 +3,6 @@ import {checkZodObjectId} from "@src/lib/index.js";
 
 export const CourseUserSchema = z.object({
     course: checkZodObjectId("course"),
-    user: checkZodObjectId("user"),
-    price: z.number().min(0),
 });
 
 export type CourseUserInput = z.infer<typeof checkZodObjectId>;
