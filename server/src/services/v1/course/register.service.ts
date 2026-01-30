331ba2990d87b074b9be01727d0b6eda0174f1ae
homow <homowsh897@gmail.com>
2026-01-30 13:30:38 +0330

    feat(response) add new response to register.services (409-500)

--- server/src/services/v1/course/register.service.ts
+++ server/src/services/v1/course/register.service.ts
@@ -1,47 +1,69 @@
 import CourseUserModel from "@models/course-user.model.js";
 import {checkCourseExist} from "@services/v1/course/get.service.js";
-import type {AuthPayload, CoursePopulate, ServiceResponse} from "@src/types/index.js";
+import type {AuthPayload, CoursePopulate, CustomError, ServiceResponse} from "@src/types/index.js";
 
 export async function registerService(
     userPayload: AuthPayload,
     courseID: string,
 ): Promise<ServiceResponse> {
-    const courseExist = await checkCourseExist({id: courseID});
+    try {
+        const courseExist = await checkCourseExist({id: courseID});
 
-    if (!courseExist) return {
-        status: 404,
-        data: {
-            ok: false,
-            message: "Course not exists",
-            code: "NOT_FOUND",
-        }
-    };
+        if (!courseExist) return {
+            status: 404,
+            data: {
+                ok: false,
+                message: "Course not exists",
+                code: "NOT_FOUND",
+            }
+        };
 
-    const course = courseExist as CoursePopulate;
+        const course = courseExist as CoursePopulate;
 
-    const courseUserRegister = await CourseUserModel.create({
-        course: course._id,
-        user: userPayload.id,
-        price: course.price,
-    });
+        const courseUserRegister = await CourseUserModel.create({
+            course: course._id,
+            user: userPayload.id,
+            price: course.price,
+        });
 
-    return {
-        status: 201,
-        data: {
-            ok: true,
-            message: "Course Register Success",
-            user: {
-                id: userPayload.id,
-                role: userPayload.role
-            },
-            course,
-            registerData: {
-                id: courseUserRegister.id,
-                user: userPayload.user.toString(),
-                course: courseUserRegister.course.toString(),
-                createdAt: courseUserRegister.createdAt.toISOString(),
-                updatedAt: courseUserRegister.updatedAt.toISOString(),
-            },
-        }
-    };
+        return {
+            status: 201,
+            data: {
+                ok: true,
+                message: "Course Register Success",
+                user: {
+                    id: userPayload.id,
+                    role: userPayload.role
+                },
+                course,
+                registerData: {
+                    id: courseUserRegister.id,
+                    user: userPayload.user.toString(),
+                    course: courseUserRegister.course.toString(),
+                    createdAt: courseUserRegister.createdAt.toISOString(),
+                    updatedAt: courseUserRegister.updatedAt.toISOString(),
+                },
+            }
+        };
+    } catch (e) {
+        const customError = e as CustomError;
+
+        if (customError.errorResponse.code === 1100) return {
+            status: 409,
+            data: {
+                ok: false,
+                message: "user already registered in this course",
+                code: "CONFLICT",
+            }
+        };
+
+        return {
+            status: customError.errorResponse.code,
+            data: {
+                ok: false,
+                message: customError.message || "Something went wrong",
+                code: "INTERNAL_SERVER_ERROR",
+            }
+        };
+    }
 }
