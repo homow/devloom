c5c0ae3c145b5872b4257772820e60812a47c5ee
homow <homowsh897@gmail.com>
2026-01-30 12:59:48 +0330

    feat(Model) completed model

--- server/src/models/course-user.model.ts
+++ server/src/models/course-user.model.ts
@@ -1,3 +1,29 @@
-import mongoose, {Types, type Schema, type Model} from "mongoose";
+import mongoose, {Types, type Model} from "mongoose";
+import type {CourseUserDB} from "@src/types/index.js";
 
-const CourseUserModel = new mongoose.Schema();
+const CourseUserModelSchema = new mongoose.Schema<CourseUserDB>(
+    {
+        course: {
+            type: Types.ObjectId,
+            required: true,
+            ref: "Course",
+        },
+        user: {
+            type: Types.ObjectId,
+            required: true,
+            ref: "User",
+        },
+        price: {
+            type: Number,
+            required: true,
+        },
+    },
+    {
+        timestamps: true
+    }
+);
+
+
+const CourseUserModel: Model<CourseUserDB> = mongoose.models.CourseUser || mongoose.model("CourseUser", CourseUserModelSchema, "course_user");
+
+export default CourseUserModel;
