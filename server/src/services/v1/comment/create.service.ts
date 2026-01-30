0f4ca7662329494562fd7a4bdc4c97177af938ab
homow <homowsh897@gmail.com>
2026-01-30 12:08:44 +0330

    refactor(service)

--- server/src/services/v1/comment/create.service.ts
+++ server/src/services/v1/comment/create.service.ts
@@ -27,27 +27,14 @@ export async function createService(
             stage: newComment.isReply ? commentWithParentStage : commentProjectStage,
         });
 
-        const comment = await CommentModel.aggregate(commentStage);
-
-        console.log(comment);
+        const [comment] = await CommentModel.aggregate(commentStage);
 
         return {
             status: 201,
             data: {
                 ok: true,
                 message: "comment created successfully",
-                comment: {
-                    ...newComment,
-                    _id: undefined,
-                    __v: undefined,
-                    id: newComment._id.toString(),
-                    course: newComment.course.toString(),
-                    writer: newComment.writer.toString(),
-                    parentComment: newComment.parentComment ? newComment.parentComment?.toString() : undefined,
-                    createdAt: newComment.createdAt.toISOString(),
-                    updatedAt: newComment.updatedAt.toISOString(),
-                },
-                pipeline: comment
+                comment
             }
         };
     }
