5a63c7a8cffd5069000719f7616ef85e02fe4694
homow <homowsh897@gmail.com>
2026-01-30 13:09:22 +0330

    feat(services) add new for register in courses

--- server/src/services/v1/course/index.ts
+++ server/src/services/v1/course/index.ts
@@ -1,3 +1,4 @@
 export * from "./create.service.js";
 export * from "./get.service.js";
 export * from "./delete.service.js";
+export * from "./register.service.js";
---
+++ server/src/services/v1/course/register.service.ts
@@ -0,0 +1,8 @@
+import type {AuthPayload, ServiceResponse} from "@src/types/index.js";
+
+export async function registerService(
+    userPayload: AuthPayload,
+    data
+): Promise<ServiceResponse> {
+
+}
