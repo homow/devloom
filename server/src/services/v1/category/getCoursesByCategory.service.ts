936a654134286e637522b93a20386a80f61d3ffd
homow <homowsh897@gmail.com>
2026-02-01 19:42:06 +0330

    feat(service) completed

--- server/src/services/v1/category/getCoursesByCategory.service.ts
+++ server/src/services/v1/category/getCoursesByCategory.service.ts
@@ -1,12 +1,26 @@
 import CourseModel from "@models/Course.model.js";
+import CategoryModel from "@models/Category.model.js";
 import type {ServiceResponse} from "@src/types/index.js";
 import {courseProjectStage, createPipelineStage} from "@src/aggregations/index.js";
 
 export async function getCoursesByCategoryService(
     href: string,
 ): Promise<ServiceResponse> {
+    const category = await CategoryModel.findOne({
+        href
+    }).lean();
+
+    if (!category) return {
+        status: 404,
+        data: {
+            ok: false,
+            message: "category not found",
+            code: "CATEGORY_NOT_FOUND"
+        }
+    };
+
     const pipelineStage = createPipelineStage({
-        filter: [{category: href}],
+        filter: [{category: category._id}],
         stage: courseProjectStage
     });
     const courses = await CourseModel.aggregate(pipelineStage);
