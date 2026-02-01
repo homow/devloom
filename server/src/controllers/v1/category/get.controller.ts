9b52b516ec9edfa2cd78f342a2ef48b84f2e3fd2
homow <homowsh897@gmail.com>
2026-02-01 19:31:53 +0330

    feat(controller) completed

--- server/src/controllers/v1/category/get.controller.ts
+++ server/src/controllers/v1/category/get.controller.ts
@@ -1,10 +1,11 @@
 import type {Request, Response} from "express";
-import {getService} from "@services/v1/category/index.js";
+import {getCoursesByCategoryService} from "@services/v1/category/index.js";
 
 export async function get(
-    req: Request<{ id?: string }>,
+    req: Request<{ href: string }>,
     res: Response
 ) {
-    const result = await getService(req.params.id);
+    const href: string = req.params?.href;
+    const result = await getCoursesByCategoryService(href);
     return res.status(result.status).json(result.data);
 }
