ae47eea88bfe02de89fd8a73f3fde336077b013e
homow <homowsh897@gmail.com>
2026-02-01 12:29:54 +0330

    feat(controller) add file fot get courses by category

--- server/src/controllers/v1/category/index.ts
+++ server/src/controllers/v1/category/index.ts
@@ -2,3 +2,4 @@ export * from "./create.controller.js";
 export * from "./get.controller.js";
 export * from "./edit.controller.js";
 export * from "./delete.controller.js";
+export * from "./getCoursesByCategory.controller.js";
