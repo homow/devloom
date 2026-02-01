7c5cc4e48d2d6820ad66e2c0ed6cf5cbeddbcd9c
homow <homowsh897@gmail.com>
2026-02-01 19:35:26 +0330

    feat(controller)

--- server/src/controllers/v1/category/get.controller.ts
+++ server/src/controllers/v1/category/get.controller.ts
@@ -1,11 +1,35 @@
 import type {Request, Response} from "express";
+import type {ServiceResponse} from "@src/types/index.js";
+import {getService} from "@services/v1/category/index.js";
 import {getCoursesByCategoryService} from "@services/v1/category/index.js";
 
 export async function get(
-    req: Request<{ href: string }>,
+    req: Request<{ href?: string }>,
     res: Response
 ) {
-    const href: string = req.params?.href;
-    const result = await getCoursesByCategoryService(href);
+    const href: string | undefined = req.params?.href;
+
+    const result: ServiceResponse = {
+        status: 404,
+        data: {
+            ok: false,
+            message: ""
+        }
+    };
+
+    if (href) {
+        const courses = await getCoursesByCategoryService(href);
+
+        result.data = courses.data;
+        result.status = courses.status;
+    }
+
+    const course = await getService();
+
+    result.status = course.status;
+    result.data = course.data;
+
+    console.log(result);
+
     return res.status(result.status).json(result.data);
 }
