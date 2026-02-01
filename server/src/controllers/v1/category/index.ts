b69b81597232acc386f1e743e931029a99f8c997
homow <homowsh897@gmail.com>
2026-02-01 12:46:19 +0330

    remove(controller) remove controller and feat on exist controller

--- server/src/controllers/v1/category/index.ts
+++ server/src/controllers/v1/category/index.ts
@@ -2,4 +2,3 @@ export * from "./create.controller.js";
 export * from "./get.controller.js";
 export * from "./edit.controller.js";
 export * from "./delete.controller.js";
-export * from "./getCoursesByCategory.controller.js";
