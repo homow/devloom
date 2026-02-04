e0f8a738f7864173f3d0b933fa16c236b990377e
= <=>
2026-02-04 16:58:51 +0330

    refactor

--- src/path.ts
+++ src/path.ts
@@ -6,7 +6,7 @@ export const BASE_URL: string = process.env.BASE_URL || "/api/v1";
 const __filename: string = fileURLToPath(import.meta.url);
 const __dirname: string = path.dirname(__filename);
 
-// create a path form root project
+/** create a path form root project */
 export function createPath(inputPath: string): string {
     const isDevelopment: boolean = __dirname.slice(__dirname.length - 3) === "src";
     const rootPath: string = path.resolve(__dirname, isDevelopment ? "../" : "./");
